f1f368f2066905ebdf1dfba3e4c32fc557ec7cdd
SIM <mrshsim@gmail.com>
2024-03-25 18:43:37 +0900

    add file : LinkedList_Reverse

---
+++ LinkedListExVer1/LinkedList_Insert_Head_Global.c
@@ -0,0 +1,57 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node_ {
+	int data;
+	struct Node_* pointer;
+};
+typedef struct Node_ Node;
+
+Node* head;
+
+//Insert the data to front of linked list
+void Insert(int data) {
+	Node* temp = (Node*)malloc(sizeof(Node));
+
+	temp->data = data;
+
+	temp->pointer = head;
+
+	head = temp;
+}
+
+//Print numbers of current linked list
+void Print() {
+	Node* temp = head;
+
+	printf("List : ");
+
+	while (temp != NULL) {
+		printf("%d ", temp->data);
+
+		temp = temp->pointer;
+	}
+
+	printf("\n");
+}
+
+int main() {
+	int length, data;
+
+	head = NULL;
+
+	printf("Enter the length of linked list : ");
+
+	scanf("%d", &length);
+
+	for (int i = 0; i < length; i++) {
+		printf("Enter the data : ");
+
+		scanf("%d", &data);
+
+		Insert(data);
+
+		Print();
+	}
+}
---
+++ LinkedList_Reverse/LinkedList_Reverse.c
@@ -0,0 +1,79 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node_ {
+	int data;
+	struct Node_* pointer;
+};
+typedef struct Node_ Node;
+
+void Insert(Node** phead, int data) {
+	Node* temp = (Node*)malloc(sizeof(Node));
+
+	temp->data = data;
+
+	if (*phead == NULL) {
+		temp->pointer = NULL;
+	}
+	else {
+		temp->pointer = *phead;
+	}
+
+	*phead = temp;
+}
+
+void Reverse(Node** phead) {
+	Node* prev, * cur, * next;
+
+	prev = NULL;
+
+	cur = *phead;
+
+	while (cur != NULL) {
+		next = cur->pointer;
+
+		cur->pointer = prev;
+
+		prev = cur;
+
+		cur = next;
+	}
+
+	*phead = prev;
+}
+
+void Print(Node* head) {
+	printf("List : ");
+
+	while (head != NULL) {
+		printf("%d ", head->data);
+		head = head->pointer;
+	}
+
+	printf("\n");
+}
+
+int main() {
+	Node* head = NULL;
+
+	int length, data;
+
+	printf("Enter the length of linked list : ");
+
+	scanf("%d", &length);
+
+	for (int i = 0; i < length; i++) {
+		printf("Enter the data : ");
+
+		scanf("%d", &data);
+
+		Insert(&head, data);
+
+		Print(head);
+	}
+
+	Reverse(&head);
+
+	Print(head);
+}
